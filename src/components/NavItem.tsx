c9de9e2c214326f0bcf114a474e245731280d3b6
Taewook Park <ld5ehom@g.ucla.edu>
2024-10-31 22:53:31 -0700

    Setup basic UI Navbar

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,16 @@
+// Items in the navigation bar (logo, cart, etc.)
+import Link from "next/link";
+import React from "react";
+
+const NavItem = ({ mobile }: { mobile?: boolean }) => {
+    return (
+        <ul className={"text-md justify-center flex gap-4 w-full item-center mr-14 ${mobile && :flex-col h-full}"}>
+            <li className = "py-2 text-center border-b-4 cursor-pointer"><Link href = "/user">User</Link></li>
+            <li className = "py-2 text-center border-b-4 cursor-pointer"><Link href = "/admin">Admin</Link></li>
+            <li className = "py-2 text-center border-b-4 cursor-pointer"><button>Login</button></li>
+            <li className = "py-2 text-center border-b-4 cursor-pointer"><button>Log Out</button></li>
+        </ul>
+    );
+};
+
+export default NavItem;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                uclaBlue: "#2774AE", // ucla blue 
+                uclaGold: "#FFD100", // ucla gold 
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
+
 export default config;
