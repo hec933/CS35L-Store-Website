5805897829333f94e7226f8de2f2e1365696a624
gweinrod <stayrange@gmail.com>
2024-11-01 09:22:38 -0700

    Fixed Lint warnings

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
     return (
-        <ul className={"text-md justify-center flex gap-4 w-full item-center mr-14 ${mobile && :flex-col h-full}"}>
+        <ul className={`text-md justify-center flex gap-4 w-full item-center mr-14 ${mobile ? 'flex-col h-full' : ''}`}>
             <li className = "py-2 text-center border-b-4 cursor-pointer"><Link href = "/user">User</Link></li>
             <li className = "py-2 text-center border-b-4 cursor-pointer"><Link href = "/admin">Admin</Link></li>
             <li className = "py-2 text-center border-b-4 cursor-pointer"><button>Login</button></li>
