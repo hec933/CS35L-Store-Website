86983496784bef70c2bc4c85ec7ac16be1e7d2a5
Taewook Park <155722702+ld5ehom@users.noreply.github.com>
2024-11-20 17:12:50 -0800

    Seller Information on the right side of the Product Detail page
    - Added a section to display detailed information about the seller, including the shop name, profile image, product count, and follower count.
    -  Seller Review Component: Displays a single review item, including the reviewer's shop profile image, name, the content of the review, and how long ago the review was created.

--- src/components/common/Shop/index.tsx
+++ src/components/common/Shop/index.tsx
@@ -1,22 +1,34 @@
-import classNames from "classnames";
-import ShopProfileImage from "../ShopProfileImage";
-import Text from "../Text";
+import classNames from 'classnames'
+import ShopProfileImage from '../ShopProfileImage'
+import Text from '../Text'
 
 interface Props {
     // The name of the shop
-    name: string;
+    name: string
 
     // URL of the shop profile image
-    profileImageUrl?: string;
+    profileImageUrl?: string
 
     // Number of products listed in the shop
-    productCount: number;
+    productCount: number
 
     // Number of followers of the shop
-    followerCount: number;
+    followerCount: number
 
     // View type of the shop component
-    type?: "row" | "column";
+    type?: 'row' | 'column'
+
+    // Callback function triggered when the shop title area is clicked
+    handleClickTitle?: () => void
+
+    // Callback function triggered when the shop profile image area is clicked
+    handleClickProfileImage?: () => void
+
+    // Callback function triggered when the ProductCount area is clicked
+    handleClickProductCount?: () => void
+
+    // Callback function triggered when the FollowerCount area is clicked
+    handleClickFollowerCount?: () => void
 }
 
 // Component for displaying shop information
@@ -25,49 +37,75 @@ export default function Shop({
     profileImageUrl,
     productCount,
     followerCount,
-    type,
+    handleClickTitle,
+    handleClickProfileImage,
+    handleClickProductCount,
+    handleClickFollowerCount,
+    type = 'row',
 }: Props) {
     return (
         <div
             className={classNames(
-                "flex",
+                'flex',
                 {
-                    "flex-row": type === "row",
-                    "flex-col": type === "column",
+                    'flex-row': type === 'row',
+                    'flex-col': type === 'column',
                 },
-                type === "column" && "gap-1 items-center"
+                type === 'column' && 'gap-1 items-center',
             )}
         >
-            <div className="w-14">
+            <div
+                className={classNames(
+                    'w-14',
+                    handleClickProfileImage && 'cursor-pointer',
+                )}
+                onClick={handleClickProfileImage}
+            >
                 <ShopProfileImage imageUrl={profileImageUrl} />
             </div>
             <div
                 className={classNames(
-                    "flex flex-col overflow-hidden",
-                    type === "row" && "ml-3 justify-around",
-                    type === "column" && "w-full"
+                    'flex flex-col overflow-hidden',
+                    type === 'row' && 'ml-3 justify-around',
+                    type === 'column' && 'w-full',
                 )}
             >
                 <div
                     className={classNames(
-                        "truncate",
-                        type === "column" && "text-center"
+                        'truncate',
+                        type === 'column' && 'text-center',
+                        handleClickTitle && 'cursor-pointer',
                     )}
+                    onClick={handleClickTitle}
                 >
                     <Text>{name}</Text>
                 </div>
                 <Text
                     size="sm"
-                    color={type === "row" ? "grey" : "black"}
+                    color={type === 'row' ? 'grey' : 'black'}
                     className={classNames(
-                        "flex gap-2",
-                        type === "column" && "justify-center"
+                        'flex gap-2',
+                        type === 'column' && 'justify-center',
                     )}
                 >
-                    <div>Products {productCount.toLocaleString()}</div> |
-                    <div>Followers {followerCount.toLocaleString()}</div>
+                    <div
+                        className={classNames(
+                            handleClickProductCount && 'cursor-pointer',
+                        )}
+                        onClick={handleClickProductCount}
+                    >
+                        Products {productCount.toLocaleString()}
+                    </div>
+                    <div
+                        className={classNames(
+                            handleClickFollowerCount && 'cursor-pointer',
+                        )}
+                        onClick={handleClickFollowerCount}
+                    >
+                        Followers {followerCount.toLocaleString()}
+                    </div>
                 </Text>
             </div>
         </div>
-    );
+    )
 }
---
+++ src/repository/followers/getIsFollowedByShopId.ts
@@ -0,0 +1,10 @@
+type Params = {
+    followerId: string
+    followedId: string
+}
+export async function getIsFollowedByShopId({
+    followerId,
+    followedId,
+}: Params): Promise<{ data: boolean }> {
+    return Promise.resolve({ data: true })
+}
