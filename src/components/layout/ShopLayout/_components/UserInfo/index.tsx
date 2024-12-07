9ea72bbb097feae165d688b2b80e3268eb733570
Greg <stayrange@gmail.com>
2024-12-07 01:01:54 -0800

    Update index.tsx
    removed duplicate login links

--- src/components/layout/ShopLayout/_components/UserInfo/index.tsx
+++ src/components/layout/ShopLayout/_components/UserInfo/index.tsx
@@ -1,16 +1,34 @@
-import Text from '@/components/common/Text'
+import { useEffect, useState } from 'react'
+import Login from './_components/Login'
 import Container from '@/components/layout/Container'
 import Wrapper from '@/components/layout/Wrapper'
-import Login from './_components/Login'
 
-// Login / Sign up
 export default function UserInfo() {
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    useEffect(() => {
+        const loginState = localStorage.getItem('loggedIn') === 'true'
+        setLoggedIn(loginState)
+    }, [])
+
     return (
         <Wrapper>
             <aside className="border-b bg-lightestBlue border-lightestBlue">
                 <Container>
                     <div className="flex justify-end py-1">
-                        <Login />
+                        {loggedIn ? (
+                            <button
+                                onClick={() => {
+                                    setLoggedIn(false)
+                                    localStorage.setItem('loggedIn', 'false')
+                                }}
+                                className="cursor-pointer hover:text-blue-500"
+                            >
+                                Log out
+                            </button>
+                        ) : (
+                            <Login />
+                        )}
                     </div>
                 </Container>
             </aside>
