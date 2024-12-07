2c739e89364c118a6b62e5345ab37ce3ed79e10e
Greg <stayrange@gmail.com>
2024-12-07 01:08:01 -0800

    Update index.tsx
    changed reference for logged in state to local storage

--- src/components/layout/ShopLayout/_components/UserInfo/index.tsx
+++ src/components/layout/ShopLayout/_components/UserInfo/index.tsx
@@ -4,34 +4,37 @@ import Container from '@/components/layout/Container'
 import Wrapper from '@/components/layout/Wrapper'
 
 export default function UserInfo() {
-    const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false)
 
-    useEffect(() => {
-        const loginState = localStorage.getItem('loggedIn') === 'true'
-        setLoggedIn(loginState)
-    }, [])
+  useEffect(() => {
+    // Only check login state once when the component mounts
+    const loginState = localStorage.getItem('loggedIn') === 'true'
+    setLoggedIn(loginState)
+  }, [])
 
-    return (
-        <Wrapper>
-            <aside className="border-b bg-lightestBlue border-lightestBlue">
-                <Container>
-                    <div className="flex justify-end py-1">
-                        {loggedIn ? (
-                            <button
-                                onClick={() => {
-                                    setLoggedIn(false)
-                                    localStorage.setItem('loggedIn', 'false')
-                                }}
-                                className="cursor-pointer hover:text-blue-500"
-                            >
-                                Log out
-                            </button>
-                        ) : (
-                            <Login />
-                        )}
-                    </div>
-                </Container>
-            </aside>
-        </Wrapper>
-    )
+  const handleLogout = () => {
+    localStorage.setItem('loggedIn', 'false')
+    setLoggedIn(false)
+  }
+
+  return (
+    <Wrapper>
+      <aside className="border-b bg-lightestBlue border-lightestBlue">
+        <Container>
+          <div className="flex justify-end py-1">
+            {loggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="cursor-pointer hover:text-blue-500"
+              >
+                Log out
+              </button>
+            ) : (
+              <Login />
+            )}
+          </div>
+        </Container>
+      </aside>
+    </Wrapper>
+  )
 }
