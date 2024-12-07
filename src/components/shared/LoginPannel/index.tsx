e45efd419b8f3c51bb50c60c47f74e129e37ff95
Greg <stayrange@gmail.com>
2024-12-07 01:11:00 -0800

    Update index.tsx
    cleaned up type state and signup component (not used)

--- src/components/shared/LoginPannel/index.tsx
+++ src/components/shared/LoginPannel/index.tsx
@@ -1,37 +1,23 @@
 import { useState } from 'react'
-
-import Login from './_components/Login'
-import SignUp from './_components/SignUp'
-
 import Button from '@/components/common/Button'
 import Text from '@/components/common/Text'
 
 
-
-// Define the props type for LoginPanel
 interface LoginPanelProps {
     handleLogin: () => void; // Function that takes no arguments and returns void
 }
 
-//hello world
-export default function LoginPanel({ handleLogin} : LoginPanelProps) {
-    const [type, setType] = useState<undefined | 'login' | 'signup'>()
-
+export default function LoginPanel({ handleLogin }: LoginPanelProps) {
     return (
         <div
             className="bg-lightestBlue flex flex-col justify-center items-center p-10 rounded w-112 gap-4"
             onClick={(e) => e.stopPropagation()}
         >
-            <Text 
-            size="lg" 
-            color="black">
+            <Text size="lg" color="black">
                 Welcome !
             </Text>
 
-            <Text 
-            size="md" 
-            weight="light" 
-            color="black">
+            <Text size="md" weight="light" color="black">
                 Sign up easily and check out the products
             </Text>
             <div className="flex flex-col gap-2 w-full">
@@ -42,7 +28,6 @@ export default function LoginPanel({ handleLogin} : LoginPanelProps) {
                     Sign in with Google
                 </Button>
             </div>
-        
         </div>
     )
 }
